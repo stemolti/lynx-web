44119b31352bbfcac9b2e42a4eff1e66ac0e4b18
stemolti <mystefano95@gmail.com>
2025-03-09 22:18:55 +0100

    Upadeted web dev configuration

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,23 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [pluginReact()],
+  server: {
+    publicDir: [
+      {
+        name: path.join(
+          __dirname,
+          '../',
+          // Please replace this with your actual Lynx project name
+          'lynx-project',
+          'dist',
+        ),
+      },
+    ],
+  },
 });
